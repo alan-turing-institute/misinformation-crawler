e92dd6bd496a262621d459600ccb91b2914ca232
Martin O'Reilly <developer@mtspace.net>
2018-07-02 19:39:52 +0100

    Add palmerreport.com spider (uses XPath link extraction)

--- misinformation/spiders/spiders.py
+++ misinformation/spiders/spiders.py
@@ -1,7 +1,7 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-
+# Crawlers that have page links on the start URL
 class ConservativeHq(CrawlSpider):
     name = 'conservativehq.com'
     allowed_domains = ['conservativehq.com']
@@ -11,6 +11,24 @@ class ConservativeHq(CrawlSpider):
         Rule(LinkExtractor(allow=('exclusives', ),)),
         # Extract links to articles
         Rule(LinkExtractor(allow=('article/',)), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        with open('article_urls/{}.txt'.format(self.name), 'a') as f:
+            # write out the title and add a newline.
+            f.write(response.url + "\n")
+            print(response.url)
+
+
+class PalmerReport(CrawlSpider):
+    name = 'palmerreport.com'
+    allowed_domains = ['palmerreport.com']
+    start_urls = ['https://palmerreport.com/category/analysis/']
+    rules = (
+        # Extract links to other pages and follow links from them (no callback means follow=True by default)
+        Rule(LinkExtractor(allow=('category/analysis/page/',), )),
+        # Extract links to articles
+        Rule(LinkExtractor(restrict_xpaths=('//h2[@class="fl-post-title"]/a',)), callback='parse_item'),
     )
 
     def parse_item(self, response):
