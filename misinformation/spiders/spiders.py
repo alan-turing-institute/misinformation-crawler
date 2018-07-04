a57e84f0a5bc40a2281b2c8654c77faff67540d3
Martin O'Reilly <developer@mtspace.net>
2018-07-04 17:00:14 +0100

    Add verteransnewsreport spider

--- misinformation/spiders/spiders.py
+++ misinformation/spiders/spiders.py
@@ -295,3 +295,20 @@ class MadWorldNews(CrawlSpider):
             f.write(response.url + "\n")
             print(response.url)
 
+
+class VeteransNewsReport(CrawlSpider):
+    name = 'veteransnewsreport.com'
+    allowed_domains = ['veteransnewsreport.com']
+    start_urls = ['http://veteransnewsreport.com/category/government-and-politics/']
+    rules = (
+        # Extract links to other pages and follow links from them (no callback means follow=True by default)
+        Rule(LinkExtractor(allow=('category/government-and-politics/page/',), )),
+        # Extract links to articles
+        Rule(LinkExtractor(restrict_xpaths=('//h3[contains(concat(" ",normalize-space(@class)," ")," entry-title ")]/a',)), callback='parse_item'),
+    )
+
+    def parse_item(self, response):
+        with open('article_urls/{}.txt'.format(self.name), 'a') as f:
+            # write out the title and add a newline.
+            f.write(response.url + "\n")
+            print(response.url)
