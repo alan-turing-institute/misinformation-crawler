37d73718789a936ab632216263ec06ee8bbf265d
Martin O'Reilly <developer@mtspace.net>
2018-11-02 03:14:30 +0000

    Ensure that 2-digit years don't partially match 4-digit years

--- tests/test_article_extraction.py
+++ tests/test_article_extraction.py
@@ -476,10 +476,46 @@ def test_extract_datetime_us_format_without_timezone():
     assert iso_string == expected_iso_string
 
 
-def test_extract_datetime_byline_conservativehq_com():
+def test_extract_datetime_byline_mmddyy_with_mmddyy_format():
     datetime_string = 'CHQ Staff | 10/17/18'
     format_string = 'MM/DD/YY'
     iso_string = extract_datetime_string(datetime_string, format_string)
     expected_iso_string = '2018-10-17T00:00:00'
+
+    assert iso_string == expected_iso_string
+
+
+def test_extract_datetime_byline_mmddyyyy_with_mmddyy_format():
+    datetime_string = 'CHQ Staff | 10/17/2018'
+    format_string = 'MM/DD/YY'
+    iso_string = extract_datetime_string(datetime_string, format_string)
+    expected_iso_string = '2018-10-17T00:00:00'
+
+    assert iso_string == expected_iso_string
+
+
+def test_extract_datetime_byline_mdyy_with_mdyy_format():
+    datetime_string = 'CHQ Staff | 1/7/18'
+    format_string = 'M/D/YY'
+    iso_string = extract_datetime_string(datetime_string, format_string)
+    expected_iso_string = '2018-01-07T00:00:00'
+
+    assert iso_string == expected_iso_string
+
+
+def test_extract_datetime_byline_0m0dyy_with_mdyy_format():
+    datetime_string = 'CHQ Staff | 01/07/18'
+    format_string = 'M/D/YY'
+    iso_string = extract_datetime_string(datetime_string, format_string)
+    expected_iso_string = '2018-01-07T00:00:00'
+
+    assert iso_string == expected_iso_string
+
+
+def test_extract_datetime_byline_mmddyy_with_mdyy_format():
+    datetime_string = 'CHQ Staff | 12/17/18'
+    format_string = 'M/D/YY'
+    iso_string = extract_datetime_string(datetime_string, format_string)
+    expected_iso_string = '2018-12-17T00:00:00'
 
     assert iso_string == expected_iso_string
