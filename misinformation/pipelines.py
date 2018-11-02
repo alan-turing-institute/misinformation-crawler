31408a4a216390151556b9b62591cb9209d2c655
Martin O'Reilly <developer@mtspace.net>
2018-11-02 03:43:12 +0000

    Update database exported INSERT SQL to reflect new data model
    Also targets fresh articles_v3 database

--- misinformation/pipelines.py
+++ misinformation/pipelines.py
@@ -52,18 +52,19 @@ class ArticleDatabaseExporter(object):
         self.insert_row_sql = """DECLARE @JSON NVARCHAR(MAX) = ?
 
 INSERT INTO [articles_v3]
-    SELECT crawl_id, crawl_date, site_name, article_url, title, author, publication_date,
-    plain_content, structured_content, metadata FROM OPENJSON (@JSON) 
+    SELECT crawl_id, crawl_datetime, site_name, article_url, title, byline, publication_datetime,
+    content, plain_content, plain_text, metadata FROM OPENJSON (@JSON) 
     WITH(
     crawl_id UNIQUEIDENTIFIER,
-    crawl_date DATETIMEOFFSET(7),
+    crawl_datetime DATETIMEOFFSET(7),
     site_name NVARCHAR(800),
     article_url NVARCHAR(800),
     title NVARCHAR(max),
-    author NVARCHAR(max),
-    publication_date DATETIME2(7),
-    plain_content NVARCHAR(max) AS JSON,
-    structured_content NVARCHAR(max) AS JSON,
+    byline NVARCHAR(max),
+    publication_datetime DATETIME2(7),
+    content NVARCHAR(max),
+    plain_content NVARCHAR(max),
+    plain_text NVARCHAR(max) AS JSON,
     metadata NVARCHAR(max) AS JSON)"""
 
     @classmethod
