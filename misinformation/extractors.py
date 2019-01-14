c1511f48ca0b6de6677df8d0e8e0b282fd0311ea
James Robinson <james.em.robinson@gmail.com>
2019-01-14 16:36:31 +0000

    Updated to ReadabiliPy 3ca447

--- misinformation/extractors.py
+++ misinformation/extractors.py
@@ -2,7 +2,7 @@ import arrow
 import logging
 from misinformation.items import Article
 import pendulum
-from ReadabiliPy import readability
+from ReadabiliPy.readabilipy import parse_to_json
 import warnings
 
 
@@ -197,13 +197,13 @@ def extract_article(response, config, crawl_info=None, content_digests=False, no
             # Extract article content from specified element
             article_html = extract_element(response, config['article']['content'])
             if article_html is not None:
-                custom_readability_article = readability.parse(article_html, content_digests, node_indexes)
+                custom_readability_article = parse_to_json(article_html, content_digests, node_indexes, False)
                 article["content"] = custom_readability_article["content"]
                 article["plain_content"] = custom_readability_article["plain_content"]
                 article["plain_text"] = custom_readability_article["plain_text"]
     # ... otherwise simply use the default values from parsing the whole page 
     else:
-        default_readability_article = readability.parse(page_html, content_digests, node_indexes)
+        default_readability_article = parse_to_json(page_html, content_digests, node_indexes, False)
         article["title"] = default_readability_article["title"]
         article["byline"] = default_readability_article["byline"]
         article["content"] = default_readability_article["content"]
