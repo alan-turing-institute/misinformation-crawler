e3d6507f01d94c202c1e51ca88a1daf4d26c6c2d
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-07-04 15:21:16 +0100

    dont expose simplify functions at module level

--- misinformation/extractors/__init__.py
+++ misinformation/extractors/__init__.py
@@ -1,7 +1,7 @@
 """
 This module contains functions for extracting data from webpages
 """
-from .extract_article import extract_article, xpath_extract_spec, simplify_extracted_byline, simplify_extracted_title
+from .extract_article import extract_article, xpath_extract_spec
 from .extract_datetime import extract_datetime_string
 from .extract_element import extract_element
 
