d2e1ce89ac2e2dbb426fb80b6f2053f535893690
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2019-07-04 14:45:12 +0100

    add comment accidentally deleted and remove unused format

--- misinformation/extractors/extract_article.py
+++ misinformation/extractors/extract_article.py
@@ -74,6 +74,7 @@ def extract_article(response, config, db_entry=None, content_digests=False, node
             iso_string = None
             if "datetime_formats" in config["article"]["publication_datetime"]:
                 datetime_formats = config["article"]["publication_datetime"]['datetime_formats']
+                # Only one format should match, so we just use the first one in the list that does
                 for dt_format in datetime_formats:
                     iso_string = extract_datetime_string(datetime_string, dt_format)
                     if iso_string:
